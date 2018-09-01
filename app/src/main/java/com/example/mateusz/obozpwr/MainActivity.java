786886159940f421d7f4e0e805e913d842b56d37
Mateusz <mattheus.zet@gmail.com>
2018-09-01 19:50:33 +0200

    Login and logout

--- app/src/main/java/com/example/mateusz/obozpwr/MainActivity.java
+++ app/src/main/java/com/example/mateusz/obozpwr/MainActivity.java
@@ -28,11 +28,40 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //username textView
+        TextView txtUsername = (TextView)findViewById(R.id.username);
+        txtUsername.setText(user.getEmail());
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+                else{
+
+                    Log.i("AuthStateChanged", "User is signed in with uid: " + user.getUid() + "User is signed in with Email: " + user.getEmail() );
+                }
+            }
+        };
 
         //calendar for date and time
 
@@ -591,6 +620,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //button WYLOGUJ
+        final Button btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    signOut();
+                }
+            });
 
 
 
@@ -712,5 +749,33 @@ public class MainActivity extends AppCompatActivity {
         return etText.getText().toString().trim().length() == 0;
     }
 
+    //sign out method
+    public void signOut() {
+        auth.signOut();
+        Toast.makeText(MainActivity.this, "WYLOGOWANIE", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
 
 }
+
+
+
